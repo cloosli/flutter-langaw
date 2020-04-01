80ccacdfa59fc84da26c5cace7129a02c09715f6
Christian Loosli <loosli.christian@gmail.com>
2020-04-01 14:36:19 -0700

    code organization

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,43 @@ void main() async {
 
   SharedPreferences storage = await SharedPreferences.getInstance();
 
+  await _loadImages();
+
+  Flame.audio.disableLog();
+  await _loadAudio();
+
+  LangawGame game = LangawGame(storage);
+  runApp(game.widget);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  flameUtil.addGestureRecognizer(tapper);
+}
+
+Future _loadAudio() async {
+  await Flame.audio.loadAll(<String>[
+    'bgm/home.mp3',
+    'bgm/playing.mp3',
+    'sfx/haha1.ogg',
+    'sfx/haha2.ogg',
+    'sfx/haha3.ogg',
+    'sfx/haha4.ogg',
+    'sfx/haha5.ogg',
+    'sfx/ouch1.ogg',
+    'sfx/ouch2.ogg',
+    'sfx/ouch3.ogg',
+    'sfx/ouch4.ogg',
+    'sfx/ouch5.ogg',
+    'sfx/ouch6.ogg',
+    'sfx/ouch7.ogg',
+    'sfx/ouch8.ogg',
+    'sfx/ouch9.ogg',
+    'sfx/ouch10.ogg',
+    'sfx/ouch11.ogg',
+  ]);
+}
+
+Future _loadImages() async {
   await Flame.images.loadAll(<String>[
     'bg/backyard.png',
     'bg/lose-splash.png',
@@ -45,33 +82,4 @@ void main() async {
     'ui/icon-sound-enabled.png',
     'ui/start-button.png',
   ]);
-
-  Flame.audio.disableLog();
-  await Flame.audio.loadAll(<String>[
-    'bgm/home.mp3',
-    'bgm/playing.mp3',
-    'sfx/haha1.ogg',
-    'sfx/haha2.ogg',
-    'sfx/haha3.ogg',
-    'sfx/haha4.ogg',
-    'sfx/haha5.ogg',
-    'sfx/ouch1.ogg',
-    'sfx/ouch2.ogg',
-    'sfx/ouch3.ogg',
-    'sfx/ouch4.ogg',
-    'sfx/ouch5.ogg',
-    'sfx/ouch6.ogg',
-    'sfx/ouch7.ogg',
-    'sfx/ouch8.ogg',
-    'sfx/ouch9.ogg',
-    'sfx/ouch10.ogg',
-    'sfx/ouch11.ogg',
-  ]);
-
-  LangawGame game = LangawGame(storage);
-  runApp(game.widget);
-
-  TapGestureRecognizer tapper = TapGestureRecognizer();
-  tapper.onTapDown = game.onTapDown;
-  flameUtil.addGestureRecognizer(tapper);
 }
